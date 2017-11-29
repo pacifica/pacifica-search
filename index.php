f19ac4645469cf812e7c8a8be9271aa6b2574d2e
David Brown <dmlb2000@gmail.com>
2017-11-28 17:26:33 -0800

    let's try fixing more stuff

--- index.php
+++ index.php
@@ -4,10 +4,9 @@
  *
  * @category Index.php
  * @package  Main
- * @author   David Brown
+ * @author   David Brown <dmlb2000@gmail.com>
  * @license  https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html GNU Lesser General Public License, version 2.1
  * @link     https://github.com/pacifica/pacifica-search
- *
  */
 ?>
 <!DOCTYPE html>
@@ -21,7 +20,7 @@
             <?php $x = 0; while($x <= 5): ?>
             <li>
                 <a href="/show.php?id=<?= $x ?>">
-		    <?= $x ?>
+                    <?= $x ?>
                     <? $x += 1; ?>
                 </a>
             </li>
--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -4,10 +4,9 @@
  *
  * @category Module
  * @package  Tests
- * @author   David Brown
+ * @author   David Brown <dmlb2000@gmail.com>
  * @license  https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html GNU Lesser General Public License, version 2.1
  * @link     https://github.com/pacifica/pacifica-search
- *
  */
 
 /**
@@ -15,21 +14,31 @@
  *
  * @category Class
  * @package  MyClass
- * @author   David Brown
+ * @author   David Brown <dmlb2000@gmail.com>
  * @license  https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html GNU Lesser General Public License, version 2.1
  * @link     https://github.com/pacifica/pacifica-search
- *
  */
 class WebTest extends PHPUnit_Extensions_Selenium2TestCase
 {
     protected $coverageScriptUrl = 'http://localhost:8193/phpunit_coverage.php';
 
+    /**
+     * Setup the selenium test environment with firefox
+     *
+     * This function sets up the selenium browser with firefox and sets the url to
+     * the correct localhost address for our testing environment.
+     */
     protected function setUp()
     {
         $this->setBrowser('firefox');
         $this->setBrowserUrl('http://localhost:8192/');
     }
 
+    /**
+     * Test the title of the main page to see if it's correct.
+     *
+     * This function tests the main page and checks for the title so we know it's correct.
+     */
     public function testTitle()
     {
         $this->url('http://localhost:8192/');
