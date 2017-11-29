45c2c5658a02e92e566e524ef4c5d7a6f8a40db9
David Brown <dmlb2000@gmail.com>
2017-11-28 17:03:39 -0800

    forgot the tests directory

--- travis/static-analysis.sh
+++ travis/static-analysis.sh
@@ -2,4 +2,4 @@
 
 # basic syntax check against all php files
 find application -name '*.php' | xargs -n 1 php -l
-./vendor/bin/phpcs -n --extensions=php --ignore=*/websystem/*,*/system/*,*/migrations/*,*/libraries/*,*/logs/*,*/third_party/* --standard=pacifica_php_ruleset.xml index.php
+./vendor/bin/phpcs -n --extensions=php --ignore=*/websystem/*,*/system/*,*/migrations/*,*/libraries/*,*/logs/*,*/third_party/* --standard=pacifica_php_ruleset.xml index.php tests
